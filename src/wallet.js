71b2ebc4c0cecbc5d026c55b62a0a3753620527e
frswim <chsh11825@gmail.com>
2025-06-19 13:01:34 +0800

    add new chain

--- src/wallet.js
+++ src/wallet.js
@@ -1,9 +1,10 @@
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
-import { mainnet, arbitrum, sepolia, polygon } from 'wagmi/chains';
+import { mainnet, arbitrum, sepolia, polygon, polygonAmoy } from 'wagmi/chains';
 
-const projectId = '17e75bfaa8c00c758e184a3d27137dee';
-const chains = [ mainnet, arbitrum, sepolia, polygon ];
+const projectId = '17e75bfaa8c00c758e184a3d27137dee';//Reown Appkit 的 ID(錢包整合)
+const chains = [ mainnet, arbitrum, sepolia, polygon, polygonAmoy ];//使用wagmi提供的共用節點
 
+//建立可連線的鏈
 const wagmiConfig = defaultWagmiConfig({
   chains,
   projectId,
@@ -11,6 +12,7 @@ const wagmiConfig = defaultWagmiConfig({
 
 });
 
+//建立彈窗(Appkit)樣式
 const web3Modal = createWeb3Modal({
   wagmiConfig: wagmiConfig,
   projectId,
