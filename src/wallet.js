abdc756dbaa0fc8c60156c10443c31cdda524fa0
frswim <chsh11825@gmail.com>
2025-07-01 13:02:21 +0800

    add new chain

--- src/wallet.js
+++ src/wallet.js
@@ -1,8 +1,33 @@
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
 import { mainnet, arbitrum, sepolia, polygon, polygonAmoy } from 'wagmi/chains';
+import { defineChain } from 'viem';
 
 const projectId = '17e75bfaa8c00c758e184a3d27137dee';//Reown Appkit 的 ID(錢包整合)
-const chains = [ mainnet, arbitrum, sepolia, polygon, polygonAmoy ];//使用wagmi提供的共用節點
+
+//加入新的鏈
+const kaiaChain = defineChain({
+  id: 8217,
+  name: 'Kaia',
+  nativeCurrency: {
+    name: 'Kaia',
+    symbol: 'KAIA',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://public-en.node.kaia.io'], // RPC 端點
+    },
+    public: {
+      http: ['https://public-en.node.kaia.io'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'KaiaScan', url: 'https://www.kaiascan.io/' },
+  },
+  testnet: false,
+});
+
+const chains = [ mainnet, arbitrum, sepolia, polygon, polygonAmoy, kaiaChain ];//使用wagmi提供的共用節點
 
 //建立可連線的鏈
 const wagmiConfig = defaultWagmiConfig({
